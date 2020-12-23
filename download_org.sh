193f6251ffd0d045c253116e4ab53fdaffd74b21
Mako <ckirkley@gmail.com>
2020-12-22 22:29:08 -0800

    Added bandcamp.

--- download_org.sh
+++ download_org.sh
@@ -1,16 +1,26 @@
 #!/bin/env bash
 
+export LABEL=~/sahel_label
+
 move_statement_files () {
-		find . -regex ./SS.*physpd.csv* -exec mv {} ~/sahel_label/secretly/phy \;
-		find . -regex ./SS.*digpd.csv* -exec mv {} ~/sahel_label/secretly/dig \;
-		find . -regex ./SS.*qb.xlsx* -exec mv {} ~/sahel_label/secretly/ \;
-		find . -regex ./SS.*t.*.xlsx* -exec mv {} ~/sahel_label/secretly/ \;
-		find . -regex ./SSwhfees.*.xlsx* -exec mv {} ~/sahel_label/secretly/fees \;
+		
+		# Secretly Dist files
+		find . -regex ./SS.*physpd.csv* -exec mv {} $LABEL/secretly/phy \;
+		find . -regex ./SS.*digpd.csv* -exec mv {} $LABEL/secretly/dig \;
+		find . -regex ./SS.*qb.xlsx* -exec mv {} $LABEL/secretly/ \;
+		find . -regex ./SS.*t.*.xlsx* -exec mv {} $LABEL/secretly/ \;
+		find . -regex ./SSwhfees.*.xlsx* -exec mv {} $LABEL/secretly/fees \;
+
+		# Bandcamp files 
+		find . -regex .*_bandcamp_raw_data_Sahel-Sounds.csv -exec mv {} $LABEL/bandcamp/ \; 
+  
+	
 }
 
 
+
 clean_up () {
-	rm -r __MACOSX
+	rm -rf __MACOSX
 }
 
 move_statement_files
